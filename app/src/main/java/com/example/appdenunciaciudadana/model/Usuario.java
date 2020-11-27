4819848f85f1b0aa12d255d9eceeaf519850fd96
JoseFuentesR <caca.candente@gmail.com>
2020-11-27 14:37:26 -0300

    "agregue mucho del video aunque tengo un problema que el fondo me sale en negro y no en blanco "

---
+++ app/src/main/java/com/example/appdenunciaciudadana/model/Usuario.java
@@ -0,0 +1,53 @@
+package com.example.appdenunciaciudadana.model;
+
+public class Usuario {
+    private String id;
+    private String email;
+    private String name;
+    private String phone;
+    private String uid;
+
+    public Usuario(){
+
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getUid() {
+        return uid;
+    }
+
+    public void setUid(String uid) {
+        this.uid = uid;
+    }
+}
